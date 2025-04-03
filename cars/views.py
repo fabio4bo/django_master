a8b0ef6e3d5ae061b092f41ceffd05992eb91647
Antonio Fábio <zantoniofabio@gmail.com>
2025-04-02 23:18:48 -0500

    hello world

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,9 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def hello(request):
+    html = '''
+        <h1 style="color: green;"> Hello World!</h1>
+    '''
+    return HttpResponse(html)
